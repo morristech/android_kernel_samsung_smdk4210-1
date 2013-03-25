f8184663b064a3748081a6195aa2bf873c536f08
Pranav Vashi <neobuddy89@gmail.com>
2013-03-26 00:39:32 +0530

    mmc: core: Fix possbile memory leak [2/2]

--- include/linux/mmc/host.h
+++ include/linux/mmc/host.h
@@ -331,6 +331,8 @@ struct mmc_host {
 	int			detect_change;	/* card detect flag */
 	struct wake_lock	detect_wake_lock;
 
+	const char *wlock_name;
+
 	const struct mmc_bus_ops *bus_ops;	/* current bus driver */
 	unsigned int		bus_refs;	/* reference counter */
 
